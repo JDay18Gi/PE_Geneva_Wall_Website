25bd694f5c687987562cac4f8e83e67f6d541602
Jason D. G <JDay18Gi@users.noreply.github.com>
2015-06-07 23:30:13 -0400

    minor style and positioning adjustments

--- genJS/genJS.js
+++ genJS/genJS.js
@@ -26,36 +26,10 @@ $(document).ready(function() {
 		});
 
 		google.maps.event.addListener(marker, 'click', function() {
-			infowindow.open(map,marker);
+			infowindow.open(map, marker);
 		});
 	}
 
+	google.maps.event.addDomListener(window, 'load', initialize);
 
-		google.maps.event.addDomListener(window, 'load', initialize);
-
-	});
-
-
-/*
-$(document).ready(function() {
-
-	function initialize() {
-		var myLatlng = new google.maps.LatLng(46.200517, 6.145845);
-		var mapOptions = {
-			zoom: 4,
-			center: myLatlng
-			}
-
-		var map = new google.maps.Map(document.getElementById('gMap'), mapOptions);
-
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map,
-			title: 'Hello World!'
-		});
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-	});
-*/
+});
