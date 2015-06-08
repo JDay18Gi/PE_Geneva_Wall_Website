b1cb6b11cd47ae1671454916f3607692f5b60612
Jason D. G <JDay18Gi@users.noreply.github.com>
2015-06-07 23:06:19 -0400

    add g-map of Geneva

---
+++ genJS/genJS.js
@@ -0,0 +1,61 @@
+
+
+
+$(document).ready(function() {
+
+	var map;
+	function initialize() {
+		var myLatlng = new google.maps.LatLng(46.200517, 6.145845);
+		var mapOptions = {
+			zoom: 13,
+			center: myLatlng
+		};
+
+		map = new google.maps.Map(document.getElementById('gMap'), mapOptions);
+
+		var infoString = "Reformation wall"+"\n Geneva, Switzerland";
+
+		var infowindow = new google.maps.InfoWindow({
+			content: infoString
+		});
+
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			title: 'Reformation Wall'
+		});
+
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(map,marker);
+		});
+	}
+
+
+		google.maps.event.addDomListener(window, 'load', initialize);
+
+	});
+
+
+/*
+$(document).ready(function() {
+
+	function initialize() {
+		var myLatlng = new google.maps.LatLng(46.200517, 6.145845);
+		var mapOptions = {
+			zoom: 4,
+			center: myLatlng
+			}
+
+		var map = new google.maps.Map(document.getElementById('gMap'), mapOptions);
+
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			title: 'Hello World!'
+		});
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+	});
+*/
